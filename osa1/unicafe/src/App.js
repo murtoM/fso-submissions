d516e9a3453d07c90fe71884fbdf8d8ecf6d370a
Markus Murto <markus@murto.eu>
2022-11-14 17:09:06 +0200

    Add progress: 1.6 unicafe step1

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedbackProps = {
+    goodProps: {
+      value: good,
+      setter: setGood,
+      uiText: "good",
+    },
+    neutralProps: {
+      value: neutral,
+      setter: setNeutral,
+      uiText: "neutral",
+    },
+    badProps: {
+      value: bad,
+      setter: setBad,
+      uiText: "bad",
+    },
+  };
+
+  const getIncreaseByOne = (value, setter) => () => setter(value + 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ButtonSet
+        buttonProps={feedbackProps}
+        onClickHandler={getIncreaseByOne}
+      />
+      <Statistics feedbackProps={feedbackProps} />
     </div>
   );
-}
+};
+
+const ButtonSet = ({ buttonProps, onClickHandler }) => {
+  return (
+    <>
+      {Object.keys(buttonProps).map((type) => {
+        return (
+          <Button
+            onClickHandler={onClickHandler(
+              buttonProps[type].value,
+              buttonProps[type].setter
+            )}
+            uiText={buttonProps[type].uiText}
+            key={type}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+const Button = ({ onClickHandler, uiText }) => {
+  return <button onClick={onClickHandler}>{uiText}</button>;
+};
+
+const Statistics = ({ feedbackProps }) => {
+  return (
+    <p>
+      {Object.keys(feedbackProps).map((type) => {
+        return (
+          <div key={type}>
+            {feedbackProps[type].uiText} {feedbackProps[type].value}
+            <br />
+          </div>
+        );
+      })}
+    </p>
+  );
+};
 
 export default App;
