392ad95a5c123cacc900224529226631bff2e64e
Markus Murto <markus@murto.eu>
2022-11-15 13:52:05 +0200

    Add progress: finish anekdootit app and osa1

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -12,22 +12,51 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0));
 
   return (
     <div>
+      <h2>Anecdote of the day</h2>
       <div>{anecdotes[selected]}</div>
+      <div>has {points[selected]} votes</div>
+      <VoteButton points={points} setPoints={setPoints} selected={selected} />
       <RandomiserButton
         setSelected={setSelected}
         anecdoteCount={anecdotes.length}
       />
+      <h2>Anecdote with most votes</h2>
+      <MostVotedAnecdote anecdotes={anecdotes} points={points} />
     </div>
   );
+};
+
+const VoteButton = ({ points, setPoints, selected }) => {
+  const voteSelected = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
+  };
+
+  return <button onClick={voteSelected}>vote</button>;
 };
 
 const RandomiserButton = ({ setSelected, anecdoteCount }) => {
   const randomiseNext = () =>
     setSelected(Math.floor(Math.random() * anecdoteCount));
+
   return <button onClick={randomiseNext}>next anecdot</button>;
+};
+
+const MostVotedAnecdote = ({ anecdotes, points }) => {
+  const mostVotedIndex = points.findIndex((point) => point === Math.max(...points));
+
+  return (
+    <div>
+      {anecdotes[mostVotedIndex]}
+      <br />
+      has {points[mostVotedIndex]} votes
+    </div>
+  );
 };
 
 export default App;
