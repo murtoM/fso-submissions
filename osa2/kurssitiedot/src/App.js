513471a952eab59c71a92cc29227d3263437c257
Markus Murto <markus@murto.eu>
2022-11-17 13:38:54 +0200

    Copy kurssitiedot app over to new part directory

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,63 @@
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((part) => {
+        return <Part part={part} />;
+      })}
+    </div>
+  );
+};
+
+const Part = (props) => {
+  const name = props.part.name;
+  const num = props.part.exercises;
+
+  return (
+    <p>
+      {name} {num}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  let total = 0;
+  props.parts.forEach((part) => {
+    total += part.exercises;
+  });
+
+  return <p>Number of exercises {total}</p>;
+};
+
+export default App;
