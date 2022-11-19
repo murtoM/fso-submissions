a6ba52d82bec9b31c90586d2bf5735fe824ea7f3
Markus Murto <markus@murto.eu>
2022-11-19 13:14:22 +0200

    Add progress: 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1231244" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const SERVER_URL = "http://localhost";
+  const SERVER_PORT = 3001;
+
+  const [persons, setPersons] = useState([]);
+  useEffect(() => {
+    axios.get(`${SERVER_URL}:${SERVER_PORT}/persons`).then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
