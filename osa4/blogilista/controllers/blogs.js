e6ce847c0907e3b08c4023e771b8a246335ec338
Markus Murto <markus@murto.eu>
2023-04-29 00:27:24 +0300

    Progress 4.22 - 4.23

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,8 +1,7 @@
 const blogsRouter = require("express").Router();
-const jwt = require("jsonwebtoken");
 
 const Blog = require("../models/blog");
-const User = require("../models/user");
+const { userExtractor } = require("../utils/middleware");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", {
@@ -13,14 +12,9 @@ blogsRouter.get("/", async (request, response) => {
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response) => {
+blogsRouter.post("/", userExtractor, async (request, response) => {
   const body = request.body;
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token invalid" });
-  }
-
-  const user = await User.findById(decodedToken.id);
+  const user = await request.user;
 
   const blog = new Blog({
     title: body.title,
@@ -36,15 +30,11 @@ blogsRouter.post("/", async (request, response) => {
   response.status(201).json(savedBlog);
 });
 
-blogsRouter.delete("/:id", async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token invalid" });
-  }
-
+blogsRouter.delete("/:id", userExtractor, async (request, response) => {
+  const user = await request.user;
   const blog = await Blog.findById(request.params.id);
 
-  if (blog.user.toString() === decodedToken.id.toString())
+  if (blog.user.toString() === user.id.toString())
     await Blog.findByIdAndDelete(request.params.id);
   else response.status(401).end();
 
