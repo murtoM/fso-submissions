667f809aa8ae66d61b4578836ec7d4c5ebc7e12f
Markus Murto <markus@murto.eu>
2023-04-29 14:31:00 +0300

    Progress 5.7

--- osa5/blogilista/frontend/src/components/BlogList.js
+++ osa5/blogilista/frontend/src/components/BlogList.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const BlogList = ({ blogs }) => (
   <div>
     <h2>blogs</h2>
@@ -7,10 +9,36 @@ const BlogList = ({ blogs }) => (
   </div>
 );
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+  const buttonLabel = detailsVisible ? "hide" : "view";
+
+  const handleClick = (event) => {
+    setDetailsVisible(!detailsVisible);
+  };
+
+  const blogStyle = {
+    paddintTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{" "}
+      <button onClick={handleClick}>{buttonLabel}</button>
+      <div style={{ display: detailsVisible ? "" : "none" }}>
+        {blog.url}
+        <br />
+        {blog.likes} <button>like</button>
+        <br />
+        {blog.user.name}
+        <br />
+      </div>
+    </div>
+  );
+};
 
 export default BlogList;
