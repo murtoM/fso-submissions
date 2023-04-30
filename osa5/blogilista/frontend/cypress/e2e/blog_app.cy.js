6b92d3cf15d7cb6f080e1dd9d0e0288c54c66f9e
Markus Murto <markus@murto.eu>
2023-04-30 16:27:58 +0300

    Progress 5.19

--- osa5/blogilista/frontend/cypress/e2e/blog_app.cy.js
+++ osa5/blogilista/frontend/cypress/e2e/blog_app.cy.js
@@ -39,4 +39,40 @@ describe("Blog app", () => {
       cy.get(".notification.error").contains("wrong username or password");
     });
   });
+
+  describe("When logged in", function () {
+    beforeEach(function () {
+      cy.get("input#username").type(user.username);
+      cy.get("input#password").type(user.password);
+      cy.get("button#login-button").click();
+    });
+
+    it("blog form is visible once 'create new blog' button is pressed", function () {
+      cy.contains("button", "create new blog").click();
+
+      cy.get("input#author").should("be.visible");
+      cy.get("input#title").should("be.visible");
+      cy.get("input#url").should("be.visible");
+    });
+
+    it("a blog can ge created", function () {
+      const blog = {
+        author: "Test author",
+        title: "Test title",
+        url: "http://domain.example/blog",
+      };
+
+      cy.contains("button", "create new blog").click();
+      cy.get("input#author").type(blog.author);
+      cy.get("input#title").type(blog.title);
+      cy.get("input#url").type(blog.url);
+      cy.contains("button", "save").click();
+
+      cy.get(".notification").contains(
+        `a new blog ${blog.title} by ${blog.author} added`
+      );
+      cy.get(".blog").contains(blog.author);
+      cy.get(".blog").contains(blog.title);
+    });
+  });
 });
